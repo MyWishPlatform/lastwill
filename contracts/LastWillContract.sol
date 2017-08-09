d558314279a3557ff37002b31a43f9bf8eb42b48
Valery Dubrava <valery.dubrava@gmail.com>
2017-08-09 18:50:29 +0300

    fix distribute logic, rewrite code a little bit (do not use storage)

--- contracts/LastWillContract.sol
+++ contracts/LastWillContract.sol
@@ -2,13 +2,13 @@ pragma solidity ^0.4.0;
 
 contract LastWillContract {
     struct RecipientPercent {
-        address recipient;
-        uint8 percent;
+    address recipient;
+    uint8 percent;
     }
 
     struct RecipientAmount {
-        address recipient;
-        uint amount;
+    address recipient;
+    uint amount;
     }
 
     // User which received all the ETH on kill or accident.
@@ -22,11 +22,9 @@ contract LastWillContract {
     function LastWillContract(address _targetUser, address[] _recipients, uint8[] _percents) {
         targetUser = _targetUser;
         lastWillAccount = msg.sender;
-        assert(_recipients.length != 0);
         assert(_recipients.length == _percents.length);
         uint8 summaryPercent = 0;
         for (uint i = 0; i < _recipients.length; i ++) {
-            assert(_percents[i] > 0);
             recipientPercents.push(RecipientPercent(_recipients[i], _percents[i]));
             summaryPercent += _percents[i];
         }
@@ -41,8 +39,9 @@ contract LastWillContract {
     // Occurs when accident happened.
     event Accident(uint balance);
     // Occurs when accident leads to sending funds to recipient.
-    event FundsSent(address indexed recipient, uint amount, uint percent);
-
+    event FundsSent(address recipient, uint amount, uint8 percent);
+    // Occurs when accident leads to sending funds to recipient.
+    event FundsChange(uint change);
 
     // ------------ EXTERNAL API ----------
     // Kill contract and return all founds to the target user.
@@ -51,67 +50,63 @@ contract LastWillContract {
         selfdestruct(targetUser);
     }
 
-    // Check and do accident if it required.
-    function check() onlyAdmin public returns (bool) {
+    function check() onlyAdmin public {
         if (doCheck()) {
             Accident(this.balance);
-            doAccident();
-            return true;
+            accident();
         }
-        return false;
     }
-
     // for debug purposes only!
-    function testDistribute(uint balance, address[] recipients, uint8[] percents)
-    returns (address[] resultRecipients, uint[] amounts, uint change) {
+    function testDistribute(uint balance, address[] recipients, uint8[] percents) {
         assert(recipients.length == percents.length);
         RecipientPercent[] memory rp = new RecipientPercent[](recipients.length);
         for (uint i = 0; i < recipients.length; i ++) {
             rp[i].recipient = recipients[i];
             rp[i].percent = percents[i];
         }
-        RecipientAmount[] storage distributed = distribute(balance, rp);
+        uint[] memory amounts = new uint[](recipients.length);
+        uint change = distribute(balance, rp, amounts);
 
-        resultRecipients = new address[](distributed.length);
-        amounts = new uint[](distributed.length);
-        change = balance;
-
-        for (uint m = 0; m < distributed.length; m ++) {
-            resultRecipients[m] = distributed[m].recipient;
-            amounts[m] = distributed[m].amount;
-            change -= distributed[m].amount;
+        for (uint m = 0; m < amounts.length; m ++) {
+            FundsSent(recipients[m], amounts[m], percents[m]);
         }
+        FundsChange(change);
     }
 
     // ------------ FALLBACK -------------
     // Must be less then 2300 gas
     function() payable {
-//        FundsAdded(msg.sender, msg.value);
+        FundsAdded(msg.sender, msg.value);
     }
 
     // ------------ INTERNAL -------------
     // Internal constant method for calculating payments.
-    function distribute(uint balance, RecipientPercent[] percents) internal constant
-        returns (RecipientAmount[] storage amounts) {
+    function distribute(uint balance, RecipientPercent[] percents, uint[] amounts) internal constant
+    returns (uint change) {
+        assert(amounts.length == percents.length);
+        change = balance;
         for (uint i = 0; i < percents.length; i ++) {
-            amounts.push(RecipientAmount(percents[i].recipient, balance * percents[i].percent / 100));
+            var amount = balance * percents[i].percent / 100;
+            amounts[i] = amount;
+            change -= amount;
         }
     }
 
-    // Do accident case.
-    function doAccident() internal {
-        RecipientAmount[] storage distributed = distribute(this.balance, recipientPercents);
+    // Do accident logic - transfer found and destruct contract.
+    function accident() internal {
+        uint[] memory amounts = new uint[](recipientPercents.length);
+        uint change = distribute(this.balance, recipientPercents, amounts);
 
-        for (uint i = 0; i < distributed.length; i ++) {
-            var amount = distributed[i].amount;
-            distributed[i].recipient.transfer(amount);
-            FundsSent(distributed[i].recipient, amount, recipientPercents[i].percent);
+        for (uint i = 0; i < recipientPercents.length; i ++) {
+            var amount = amounts[i];
+            recipientPercents[i].recipient.transfer(amount);
+            FundsSent(recipientPercents[i].recipient, amount, recipientPercents[i].percent);
         }
 
+        FundsChange(change);
         selfdestruct(targetUser);
     }
 
-    // Do check (override it).
     function doCheck() internal returns (bool);
 
     function isTarget() internal constant returns (bool) {
