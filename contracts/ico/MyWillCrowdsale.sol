bef62e54cda7ac2cce92028846bbc33ca3099d03
Valery Dubrava <valery.dubrava@gmail.com>
2017-09-15 10:09:25 +0300

    introduce rate provider

--- contracts/ico/MyWillCrowdsale.sol
+++ contracts/ico/MyWillCrowdsale.sol
@@ -1,22 +1,18 @@
 pragma solidity ^0.4.16;
 
 import "./MyWillToken.sol";
+import "./MyWillRateProvider.sol";
 import "./zeppelin/crowdsale/RefundableCrowdsale.sol";
 
 contract MyWillCrowdsale is RefundableCrowdsale {
     uint constant tokenDecimal = 18;
     uint constant tokenDecimalMultiplier = 10 ** tokenDecimal;
-    uint constant step_30 = 20000000 * tokenDecimalMultiplier;
-    uint constant step_20 = 40000000 * tokenDecimalMultiplier;
-    uint constant step_10 = 60000000 * tokenDecimalMultiplier;
-    uint16 constant rate_30 = 1950;
-    uint16 constant rate_20 = 1800;
-    uint16 constant rate_10 = 1650;
-
     uint constant teamTokens = 11000000 * tokenDecimalMultiplier;
     uint constant bountyTokens = 2000000 * tokenDecimalMultiplier;
     address constant teamAddress = 0x1;
     address constant bountyAddress = 0x2;
+
+    MyWillRateProviderI rateProvider;
 
     function MyWillCrowdsale(
             uint32 _startTime,
@@ -31,6 +27,8 @@ contract MyWillCrowdsale is RefundableCrowdsale {
         token.mint(teamAddress,  teamTokens);
         token.mint(bountyAddress, bountyTokens);
 
+        rateProvider = new MyWillRateProvider();
+
         // pre ICO
     }
 
@@ -39,35 +37,15 @@ contract MyWillCrowdsale is RefundableCrowdsale {
     }
 
     function getRate(uint value) internal constant returns (uint) {
-        uint baseRate;
-        // apply sale
-        if (soldTokens < step_30) {
-            baseRate = rate_30;
-        }
-        else if (soldTokens < step_20) {
-            baseRate = rate_20;
-        }
-        else if (soldTokens < step_10) {
-            baseRate = rate_10;
-        }
-        else {
-            baseRate = rate;
-        }
-
-        // apply bonus for amount
-        if (value >= 5000 * tokenDecimalMultiplier) {
-            baseRate += 50;
-        }
-        else if (value >= 3000 * tokenDecimalMultiplier) {
-            baseRate += 30;
-        }
-        else if (value >= 1000 * tokenDecimalMultiplier) {
-            baseRate += 10;
-        }
-        return baseRate;
+        return rateProvider.getRate(msg.sender, soldTokens, value);
     }
 
     function transferTokenOwnership(address newOwner) onlyOwner {
         token.transferOwnership(newOwner);
+    }
+
+    function setRateProvider(address _rateProviderAddress) onlyOwner {
+        require(_rateProviderAddress != 0);
+        rateProvider = MyWillRateProviderI(_rateProviderAddress);
     }
 }
---
+++ contracts/ico/MyWillRateProvider.sol
@@ -0,0 +1,84 @@
+pragma solidity ^0.4.16;
+
+import "./zeppelin/ownership/Ownable.sol";
+
+contract MyWillRateProviderI {
+    /**
+     * @dev Calculate actual rate using the specified parameters.
+     * @param buyer     Investor (buyer) address.
+     * @param totalSold Amount of sold tokens.
+     * @param amountWei Amount of wei to purchase.
+     * @return ETH to Token rate.
+     */
+    function getRate(address buyer, uint totalSold, uint amountWei) returns (uint rate);
+}
+
+contract MyWillRateProvider is MyWillRateProviderI, Ownable {
+    uint constant tokenDecimal = 18;
+    uint constant tokenDecimalMultiplier = 10 ** tokenDecimal;
+    uint constant step_30 = 20000000 * tokenDecimalMultiplier;
+    uint constant step_20 = 40000000 * tokenDecimalMultiplier;
+    uint constant step_10 = 60000000 * tokenDecimalMultiplier;
+    uint16 constant rate_30 = 1950;
+    uint16 constant rate_20 = 1800;
+    uint16 constant rate_10 = 1650;
+
+    struct ExclusiveRate {
+        // be careful, accuracies this about 15 minutes
+        uint32 workUntil;
+        // exclusive rate or 0
+        uint16 rate;
+        // additional rate or 0
+        uint16 additionalRate;
+        // flag to check, that record exists
+        bool exists;
+    }
+
+    mapping(address => ExclusiveRate) exclusiveRate;
+
+    function getRate(address buyer, uint totalSold, uint amountWei) returns (uint rate) {
+        uint baseRate;
+        // apply sale
+        if (totalSold < step_30) {
+            baseRate = rate_30;
+        }
+        else if (totalSold < step_20) {
+            baseRate = rate_20;
+        }
+        else if (totalSold < step_10) {
+            baseRate = rate_10;
+        }
+        else {
+            baseRate = rate;
+        }
+
+        // apply bonus for amount
+        // TODO
+        if (amountWei >= 5000 * tokenDecimalMultiplier) {
+            baseRate += 50;
+        }
+        else if (amountWei >= 3000 * tokenDecimalMultiplier) {
+            baseRate += 30;
+        }
+        else if (amountWei >= 1000 * tokenDecimalMultiplier) {
+            baseRate += 10;
+        }
+
+        ExclusiveRate memory eRate = exclusiveRate[buyer];
+        if (eRate.exists && eRate.workUntil >= now) {
+            if (eRate.rate != 0) {
+                baseRate = eRate.rate;
+            }
+            baseRate += eRate.additionalRate;
+        }
+        return baseRate;
+    }
+
+    function setExclusiveRate(address _investor, uint16 _rate, uint16 _additionalRate, uint32 _workUntil) onlyOwner {
+        exclusiveRate[_investor] = ExclusiveRate(_workUntil, _rate, _additionalRate, true);
+    }
+
+    function removeExclusiveRate(address _investor) onlyOwner {
+        delete exclusiveRate[_investor];
+    }
+}
