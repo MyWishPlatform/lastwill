6e5d43a7ec77a90ef23a6cca3f66913f3aeee6e6
polyglot0027 <familyl@yandex.ru>
2017-09-28 10:42:37 +0300

    + new condition to internalCheck added, ~ timestamp types changed

--- contracts/LastWillWallet.sol
+++ contracts/LastWillWallet.sol
@@ -4,19 +4,19 @@ import "LastWill.sol";
 
 contract LastWillWallet is LastWill {
 
-    uint32 private lastOwnerActivity;
-    uint32 private noActivityPeriod;
+    uint256 private lastOwnerActivity;
+    uint256 private noActivityPeriod;
 
     event Withdraw(address _sender, uint256 amount, address _beneficiary);
 
     function LastWillWallet(address _targetUser, address[] _recipients, uint8[] _percents, uint32 _noActivityPeriod)
         LastWill(_targetUser, _recipients, _percents) {
             noActivityPeriod = _noActivityPeriod;
-            lastOwnerActivity = uint32(block.timestamp);
+            lastOwnerActivity = block.timestamp;
     }
 
     function internalCheck() internal returns (bool) {
-        return (block.timestamp - lastOwnerActivity) >= noActivityPeriod;
+        return block.timestamp > lastOwnerActivity && (block.timestamp - lastOwnerActivity) >= noActivityPeriod;
     }
 
     function sendFunds(uint256 _amount, address _receiver) onlyTarget() onlyAlive() external returns (uint256) {
@@ -25,7 +25,7 @@ contract LastWillWallet is LastWill {
         require(_receiver.send(_amount));
 
         Withdraw(msg.sender, _amount, _receiver);
-        lastOwnerActivity = uint32(block.timestamp);
+        lastOwnerActivity = block.timestamp;
         return this.balance;
     }
 }
