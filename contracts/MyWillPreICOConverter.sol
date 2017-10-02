d9b6afe365f89c2985bbdc6a3d0d4cf1abf88093
Valery Dubrava <valery.dubrava@gmail.com>
2017-10-02 17:46:00 +0300

    add preico 2 ico converter

---
+++ contracts/MyWillPreICOConverter.sol
@@ -0,0 +1,164 @@
+pragma solidity ^0.4.16;
+
+import './ico/zeppelin/token/ERC20.sol';
+import './ico/zeppelin/ownership/Ownable.sol';
+
+contract AddressProvider {
+    function eachAddress(function (uint, address) returns (bool) callback) internal returns (uint) {
+        if (!callback(1, 0x4e23f54d8879805be0000351c1597eee88ba7874)) return 1;
+        if (!callback(2, 0x47ae1ec0e0d97e477c4acf077eee66fc72bd01b0)) return 2;
+        if (!callback(3, 0x82cfb71cb8d3aba8598719e97cd9153202d6e171)) return 3;
+        if (!callback(4, 0x7a2d87f5db7b0825264cc6329d95fd660952dec7)) return 4;
+        if (!callback(5, 0xede8faa3cd51379798b09ff638cd8f19fd5bd048)) return 5;
+        if (!callback(6, 0xf249fac688752acb3aaff4a9f173a2c1e6e4ca58)) return 6;
+        if (!callback(7, 0x2d97919c786f6a067b3dd3177dec0925a8193254)) return 7;
+        if (!callback(8, 0x2421550c599663c6e4551b8ba36fdbb904b4f0b7)) return 8;
+        if (!callback(9, 0x21c76b3e2c258ed0e7c44534eb9b5c7e817ea7fb)) return 9;
+        if (!callback(10, 0xe1e7f56626fea4d8fbddf642b26e67a9dba83d91)) return 10;
+        if (!callback(11, 0x787d6b93e32dea45b195ece5b613af966e1b15c9)) return 11;
+        if (!callback(12, 0x2709d69fa5f2f410899798c854dbacf2a9eede8a)) return 12;
+        if (!callback(13, 0xcd8862d38e1463cca235b29368917257a45bce39)) return 13;
+        if (!callback(14, 0x08bea4ccc9c45e506d5bc5e638acaa13fa3e801c)) return 14;
+        if (!callback(15, 0x56ffff235ae2690249e16ecbcb0ea8c917bfd194)) return 15;
+        if (!callback(16, 0xd14a86c1d73c2b583b540838ad2c541182138484)) return 16;
+        if (!callback(17, 0xa92488ffb064fc33b33eb36dee24f8088a897c76)) return 17;
+        if (!callback(18, 0x48f73dc45bd3714e8c79a93d7619a737260facfd)) return 18;
+        if (!callback(19, 0x23e5217178c6c4edcf280aa64b20e3f6d9f7621f)) return 19;
+        if (!callback(20, 0x7774901287dfa02e230d1ba95c0ae20a71793b53)) return 20;
+        if (!callback(21, 0x555cbe849bf5e01db195a81ecec1e65329fff643)) return 21;
+        if (!callback(22, 0x03e95573224d8d83811091fe53aee37cf306242e)) return 22;
+        if (!callback(23, 0x5472ddbcecd956c73a64aac115daaba16c49d8fb)) return 23;
+        if (!callback(24, 0x5f36cbfa521148a88dd615b128d900efa2592ba3)) return 24;
+        if (!callback(25, 0x0386c37be02ca51e5aedae74fec07dda6d53f93e)) return 25;
+        if (!callback(26, 0x2e876c968f0baa044636d2795ef524dd1d049407)) return 26;
+        if (!callback(27, 0x15b7fec273f6f09786ae3efc6e0aa9ea33f56508)) return 27;
+        if (!callback(28, 0x88b164e589331db3aaec043c6d8da00e36853b34)) return 28;
+        if (!callback(29, 0x0aea4192f32e9e92d77fa32595a9778790d06c93)) return 29;
+        if (!callback(30, 0xc94dc73a2677e012fb6260c680ce5344e3aeaab8)) return 30;
+        if (!callback(31, 0xa3d2cf074f529cda56727c96be5a9c270838af3c)) return 31;
+        if (!callback(32, 0x0472f4efa88b2e35fe4bea0e7c1b4abbc2ee53dd)) return 32;
+        if (!callback(33, 0x045b90f5ac7aa867ec2d156a7050d2cac06637e3)) return 33;
+        if (!callback(34, 0x902f39d092f866abdfa0320f91b47e7222a13dd9)) return 34;
+        if (!callback(35, 0x002de75b97623e8c374206f84de8a6f5d3134a10)) return 35;
+        if (!callback(36, 0x9cd9b250cdf7f1aebb2a78babeae8728b5bf1146)) return 36;
+        if (!callback(37, 0x3833f8dbdbd6bdcb6a883ff209b869148965b364)) return 37;
+        if (!callback(38, 0x5fe84442c5fc9409108ed063d5756d14a5f812f8)) return 38;
+        if (!callback(39, 0xb2136326e9f8d1d32b39b85983ed4fa8318003bf)) return 39;
+        if (!callback(40, 0x1b5cc4b7f5a0fad13de5d5676f1dd58d9b6cd28d)) return 40;
+        if (!callback(41, 0x414cc62dc5c2081eca297928b469cdddc5c8f398)) return 41;
+        if (!callback(42, 0xbafdbde706357d80151ab10310c1c7b54b36d3c0)) return 42;
+        if (!callback(43, 0xd7a738676cdf6a8dbd7d2858e8a9503989669714)) return 43;
+        if (!callback(44, 0x073aa5bde01ec3c32193f4f3980077952f6fbb3c)) return 44;
+        if (!callback(45, 0xe212ca66e9e7ae718983e320d9404ef4b21b2d6b)) return 45;
+        if (!callback(46, 0xa1ca8da0496cfa2f8579f58db30ae8ad8dbe6f0c)) return 46;
+        if (!callback(47, 0xe424ebbecc3a948d17900b5e7b8797afe35de70a)) return 47;
+        if (!callback(48, 0xe2e584319f57671638cb28321b830e4a79d47218)) return 48;
+        if (!callback(49, 0x4dfa4d34cc420a0a08e2fba956bc1527ef77e3e5)) return 49;
+        if (!callback(50, 0x9798b35cf51a135c0d0ee8e6feb0b13bb62253cf)) return 50;
+        if (!callback(51, 0x414f0e2730294c1f3c6b0262fcd42ad81400223d)) return 51;
+        if (!callback(52, 0x977f5b0964fd91c42fbb4430cce1cee11e619306)) return 52;
+        if (!callback(53, 0xb50b928a5d63d53db59d294feb34d7469a4da10b)) return 53;
+        if (!callback(54, 0x74f1c3a95e3a18427b47f0633f6c2b4459659879)) return 54;
+        if (!callback(55, 0x3dd932d51fb898374e694d764d059d2e2453e4d1)) return 55;
+        if (!callback(56, 0xdec7430b9612f37c6dee9661503cb80092835e3a)) return 56;
+        if (!callback(57, 0xd9e09aeb48907da831219574cdf682db56dd3a5c)) return 57;
+        if (!callback(58, 0x89d853091724fd07b95a928c7e1bc17238d85bf6)) return 58;
+        if (!callback(59, 0x7398a2edb928a2e179f62bfb795f292254f6850e)) return 59;
+        if (!callback(60, 0x0f5d5470a0c1f1ed2f4f76b37a0de172f0be5cf5)) return 60;
+        if (!callback(61, 0xd1f88c0d1fec2a4eef153ccc1cf2d3de7d078ccf)) return 61;
+        if (!callback(62, 0x01f5be9fcc63bb4ede40e89db317d507c5adf81b)) return 62;
+        if (!callback(63, 0x65e8d31e206c1955599b8fd3f16ade5b79609d8c)) return 63;
+        if (!callback(64, 0x00d2b4411123ccac9764a6fba6f9ec2ea6100821)) return 64;
+        if (!callback(65, 0xc30637cbbe78b103ab88442bad7a471784667a8a)) return 65;
+        if (!callback(66, 0x39606e74585d8e5d278daefaf5ebf0af7831f2a6)) return 66;
+        if (!callback(67, 0xb22f376f70f34c906a88a91f6999a0bd1a0f3c3d)) return 67;
+        if (!callback(68, 0x7dede2f1641f7a4682209174ac9d78c56f9a74c6)) return 68;
+        if (!callback(69, 0x8f35ffe6637d15967ef91ae56b957bf2ba57f340)) return 69;
+        if (!callback(70, 0xe254ef435646457e7941cb9d8fecd8a1bdc4cc40)) return 70;
+        if (!callback(71, 0x860d0f29d5d67ab1d07a35b68723e3dc8f519792)) return 71;
+        if (!callback(72, 0x92c3fc5be5a95d24d5e61d965b4902227e748ea2)) return 72;
+        if (!callback(73, 0xf1e338ffd41aa27b78daab835e354432e6c5275a)) return 73;
+        if (!callback(74, 0x362776e3f21f2ac5c6206a6b65f2a335bf64c8ca)) return 74;
+        if (!callback(75, 0x24b171184f7385701281acafc2204401dbe2af43)) return 75;
+        if (!callback(76, 0x8cd3bec93d81f900f4faef7f40259d392b616a7e)) return 76;
+        if (!callback(77, 0x3655309da47817f286788efcf59a789a2e8028dd)) return 77;
+        if (!callback(78, 0xd51080d56914c4501044f00dd9f52b6e3763d003)) return 78;
+        if (!callback(79, 0x532cbc68f66bb7482086a15972fa20ee3aca21c4)) return 79;
+        if (!callback(80, 0xdf6df43709c264ef5a7a1bc4d6c962472359a8e8)) return 80;
+        if (!callback(81, 0x16ec8a7f9d21ea94be12d4765ad2e5597f82c9fe)) return 81;
+        if (!callback(82, 0xe28c5e4c6891afb0df739910c733766305cde69a)) return 82;
+        if (!callback(83, 0x0f7c1b4485574b47e7dadff2835a26aed5082af1)) return 83;
+        if (!callback(84, 0xb3bf66e38cd47c678cae2d4b4084849b2b82cd4d)) return 84;
+        if (!callback(85, 0x49e7c0f9fd172d58d2899aa215da1d055def166c)) return 85;
+        if (!callback(86, 0x643710b40debc46de9766994e9aa749722d1e0f9)) return 86;
+        if (!callback(87, 0xf43ea4b694cc824e07716bc2a05f6cf47cbf4e4f)) return 87;
+        if (!callback(88, 0x289d2e750ac6765ab42a9e7fd8dcdd5a3402f74a)) return 88;
+        if (!callback(89, 0x55472afff5cbe9d82837b8fe4fe5ba81f5555b58)) return 89;
+        if (!callback(90, 0x836df7aa96f8163564ad755e9437336f8abe91a1)) return 90;
+        if (!callback(91, 0xaaeb456ab8063c0d13f8d5b597117426ffef59ce)) return 91;
+        if (!callback(92, 0xebff84bbef423071e604c361bba677f5593def4e)) return 92;
+        if (!callback(93, 0x346c7ebde5cf6457a0a19472eb5839493076ce20)) return 93;
+        if (!callback(94, 0x2ddc72111be3695ed38958e4275f77ea50c9ada8)) return 94;
+        if (!callback(95, 0xce14cec3d6a45294314d2bcfd70b1e9e9a723725)) return 95;
+        if (!callback(96, 0x5c621c7b10f025fc34d60192277caf350b09e5a0)) return 96;
+        if (!callback(97, 0x64646d3106116792b1ccb1e515bc75140d7cb0d7)) return 97;
+        if (!callback(98, 0xe0a36528450ddabf3a9f013a13dc7816bf0a2138)) return 98;
+        if (!callback(99, 0x58ebf540a42c2a4b6e2c89c289795d28a69150cd)) return 99;
+        if (!callback(100, 0x6ebcb55c56d13fd7208f67009f06922e5403066c)) return 100;
+        if (!callback(101, 0x482d140f0f4d17d72b4df35a9fc5d8fa1b566d4c)) return 101;
+        if (!callback(102, 0x979165d2277911b198c1c17bb04f5c9c300e6b91)) return 102;
+        if (!callback(103, 0x1b4d7ae3ff3183e4ee5972492621eca2d74fdf90)) return 103;
+        if (!callback(104, 0xbc157daee7f33cf00bbc4f2162b7799089aef0f8)) return 104;
+        if (!callback(105, 0xbfc38f9c9b75a962ed0092f5666f702fb3d19f94)) return 105;
+        if (!callback(106, 0x917256a780017ca9d82fd15fb59b9b80df8a9604)) return 106;
+        if (!callback(107, 0x787e6870754cc306c1758db62dc6cd334ba76345)) return 107;
+        if (!callback(108, 0xb1d7accb3a90fe06a5c330f9821749a9f1987acc)) return 108;
+        if (!callback(109, 0xf828e2fcb97846d30cab3fe84f6bf8dc9e4cd63c)) return 109;
+        if (!callback(110, 0xff69cf6e54d65af17d5646de6b91974c02aba2d9)) return 110;
+        if (!callback(111, 0x4975bfb3c89529efbe778bd192a941d3d42ebe59)) return 111;
+        if (!callback(112, 0x1ccf1348f958da978cad5967e52740dde08ac5a0)) return 112;
+    }
+}
+
+contract MyWillPreICOConverter is AddressProvider, Ownable {
+    address constant PRL_ADDRESS = 0x195610851A43E9685643A8F3b49F0F8a019204f1;
+    ERC20 public prlToken;
+    ERC20 public willToken;
+    uint public lastIndex;
+
+    uint summary;
+    uint limit;
+
+    function MyWillPreICOConverter(address _prlTokenAddress, address _willToken) {
+        prlToken = ERC20(_prlTokenAddress);
+        willToken = ERC20(_willToken);
+
+        eachAddress(calcSummary);
+        require(summary == prlToken.totalSupply());
+    }
+
+    function calcSummary(uint index, address adr) internal returns (bool) {
+        summary += prlToken.balanceOf(adr);
+        return true;
+    }
+
+    function sendToken(uint _limit) onlyOwner {
+        limit = _limit;
+        eachAddress(sendTokenInternal);
+    }
+
+    function sendTokenInternal(uint index, address adr) internal returns (bool) {
+        if (lastIndex >= index) {
+            return true;
+        }
+        if (index > limit) {
+            return false;
+        }
+
+        uint amount = prlToken.balanceOf(adr);
+
+        lastIndex = index;
+        require(willToken.transferFrom(PRL_ADDRESS, adr, amount));
+        return true;
+    }
+}
